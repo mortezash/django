717f6357131d4ef968f2861ad898d2b01b7a0ae3
mortezash <32642757+mortezash@users.noreply.github.com>
2026-02-09 14:04:06 +0330

    feat : Template inheritance , for  , if ,Filters

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 def home(request):
     context = {
-        'name': 'پایتون',
+        'name': 'Python',
         'age': 30,
+        'skills' : ['Python', 'Django', 'Docker']
     }
     return render(request, 'blog/home.html', context)
